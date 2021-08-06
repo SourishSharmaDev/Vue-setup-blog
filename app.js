16596fc7f9ace101d5daf00dcc3e6e4ab41c628c
SourishSharmaDev <sourishdevsharma@gmail.com>
2021-08-06 19:59:15 +0530

    scroll bar design

--- app.js
+++ app.js
@@ -2,5 +2,5 @@
 window.addEventListener('scroll', ()=>{
     let navbar = document.querySelector('.navbar');
     let winPos = window.scrollY
-    navbar.classList.toggle('navbar-scrolled', winPos > 0)
+    navbar.classList.toggle('navbar-scrolled', winPos > 20)
 })
