2c89d3c18e3b17f73cb01c27c2bac85047a70445
SourishSharmaDev <sourishdevsharma@gmail.com>
2021-08-06 13:31:28 +0530

    Navbar commit-2, making the navbar change colors when scrolled

---
+++ app.js
@@ -0,0 +1,6 @@
+
+window.addEventListener('scroll', ()=>{
+    let navbar = document.querySelector('.navbar');
+    let winPos = window.scrollY
+    navbar.classList.toggle('navbar-scrolled', winPos > 0)
+})
